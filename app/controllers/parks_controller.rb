c3580b36a8e32472718694c5edc9a69d5c99a748
Mohamed Warsame <mwarsame9@gmail.com>
2017-07-21 11:18:12 -0700

    add delete, create, update to parks controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,12 +1,40 @@
 class ParksController < ApplicationController
 
   def index
-    @parks = "Acadia National Park in Maine"
+    @parks = Park.all
     json_response(@parks)
   end
 
+  def show
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @park = Park.create!(park_params)
+    json_response(@park, :created)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    if @park.update!(park_params)
+      render status: 200, json: {
+        message: "Your park has been updated!"
+      }
+    end
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    if @park.destroy
+      render status: 200, json: {
+        message: "Your park has been deleted!"
+      }
+    end
+  end
+
   private
-  def json_response(object)
-    render json: object, status: :ok
+  def park_params
+    params.permit(:name, :location, :description, :park_type)
   end
 end
